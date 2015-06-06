c8da39da304c59f18df4f3978324787ff150a004
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2015-06-06 11:08:24 -0300

    Include bin/notify with Homebrew Formula
    Signed-off-by: Rogerio Prado de Jesus <rogeriopradoj@gmail.com>

---
+++ Formula/notify.rb
@@ -0,0 +1,15 @@
+class Notify < Formula
+  desc "notify via iTerm2 using terminal-notifier"
+  homepage "https://rogeriopradoj.github.io/homebrew-bin/"
+  url "https://github.com/rogeriopradoj/homebrew-bin/blob/master/bin/notify"
+  sha256 "b1188998dfab568928503d3a6a099d3165249066cdb630661e61a76e5f2a9d0e"
+  version "b1188998dfab568928503d3a6a099d3165249066cdb630661e61a76e5f2a9d0e"
+
+  def install
+    bin.install "notify"
+  end
+
+  test do
+    system "which", "#{bin}/notify", "&>/dev/null"
+  end
+end
