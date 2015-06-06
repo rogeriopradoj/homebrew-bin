a9f7422db77ca16b325364948056584226d2d8e4
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2015-06-06 11:39:24 -0300

    Fix url in Formula/notify.rb
    Signed-off-by: Rogerio Prado de Jesus <rogeriopradoj@gmail.com>

--- Formula/notify.rb
+++ Formula/notify.rb
@@ -1,7 +1,7 @@
 class Notify < Formula
   desc "notify via iTerm2 using terminal-notifier"
   homepage "https://rogeriopradoj.github.io/homebrew-bin/"
-  url "https://github.com/rogeriopradoj/homebrew-bin/blob/master/bin/notify"
+  url "https://raw.githubusercontent.com/rogeriopradoj/homebrew-bin/master/bin/notify"
   sha256 "b1188998dfab568928503d3a6a099d3165249066cdb630661e61a76e5f2a9d0e"
   version "0.1.0-b1188998dfab568928503d3a6a099d3165249066cdb630661e61a76e5f2a9d0e"
 
