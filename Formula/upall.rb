4562f3bbe6240989b822f2541aeac19cc082fa9f
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2015-09-29 00:45:09 -0300

    Include composer in bin/upall

--- Formula/upall.rb
+++ Formula/upall.rb
@@ -1,9 +1,9 @@
 class Upall < Formula
-  desc "Update and upgrade [oh-my-zsh] [homebrew] and brew installed things"
+  desc "Update and upgrade [oh-my-zsh], [homebrew], brew installed things, [composer] and composer global packages"
   homepage "https://rogeriopradoj.github.io/homebrew-bin/#upall"
   url "https://raw.githubusercontent.com/rogeriopradoj/homebrew-bin/master/bin/upall"
-  sha256 "d012cbf9f23797e913a4bc4761421a638c4f2b694e0b1940cfc50ea5a6b9e343"
-  version "1.0.0-beta+d012cbf9f23797e913a4bc4761421a638c4f2b694e0b1940cfc50ea5a6b9e343"
+  sha256 "43bf3ccba167468ff246fab2b1a37b396f74ca46f2da1824cf9c8e7f25dec9d3"
+  version "1.0.0-beta+43bf3ccba167468ff246fab2b1a37b396f74ca46f2da1824cf9c8e7f25dec9d3"
 
   def install
     bin.install "upall"
