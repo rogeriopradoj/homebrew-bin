d4bdf82ce509b19f3f79f1258dc3380ea018b9f2
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2015-09-29 00:06:46 -0300

    Fix --version for bin/upall and bin/utf8

--- Formula/utf8.rb
+++ Formula/utf8.rb
@@ -2,8 +2,8 @@ class Utf8 < Formula
   desc "Create a utf8 version of any file, prepending a _ (underline) in the generated file"
   homepage "https://rogeriopradoj.github.io/homebrew-bin/#utf8"
   url "https://raw.githubusercontent.com/rogeriopradoj/homebrew-bin/master/bin/utf8"
-  sha256 "09a049412c4d2a05d0c1455ef6bd6ae0d94712a5219877df64378f5a80179821"
-  version "1.0.0-beta+09a049412c4d2a05d0c1455ef6bd6ae0d94712a5219877df64378f5a80179821"
+  sha256 "aad28b57c48c8d75c398024ce0b2b64faa9e0adbbf5a8634d186756090b7ba75"
+  version "1.0.0-beta+aad28b57c48c8d75c398024ce0b2b64faa9e0adbbf5a8634d186756090b7ba75"
 
   def install
     bin.install "utf8"
