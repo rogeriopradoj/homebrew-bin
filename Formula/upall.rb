8061827d1a8fb9c2e5c3a04af033c6405ee47f5e
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2016-10-05 01:24:32 -0300

    Bump upall to 1.1.0 (upgrade mas applications)

--- Formula/upall.rb
+++ Formula/upall.rb
@@ -1,9 +1,9 @@
 class Upall < Formula
-  desc "Update and upgrade [oh-my-zsh], [homebrew], brew installed things, [composer] and composer global packages"
+  desc "Update and upgrade [oh-my-zsh], [homebrew], [mas], [composer]"
   homepage "https://rogeriopradoj.github.io/homebrew-bin/#upall"
   url "https://raw.githubusercontent.com/rogeriopradoj/homebrew-bin/master/bin/upall"
   sha256 "775770df5aab2035a07e8cd856238cbd0491868ad9957b4befa106fd2fddd380"
-  version "1.0.0"
+  version "1.1.0"
 
   def install
     bin.install "upall"
