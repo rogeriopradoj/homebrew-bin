e22d7f628a33c259efb9adfe7b04d0896289fed0
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2016-10-21 23:28:55 -0200

    Bump upall version to 1.1.1 (notice error on brew upgrade --all)

--- Formula/upall.rb
+++ Formula/upall.rb
@@ -2,8 +2,8 @@ class Upall < Formula
   desc "Update and upgrade [oh-my-zsh], [homebrew], [mas], [composer]"
   homepage "https://rogeriopradoj.github.io/homebrew-bin/#upall"
   url "https://raw.githubusercontent.com/rogeriopradoj/homebrew-bin/master/bin/upall"
-  version "1.1.0"
-  sha256 "f46d37538a771fcf207b1705a75c2c8417ef97b32bfa739c52385507de3a0800"
+  version "1.1.1"
+  sha256 "ac132c0f30a8aa90be216ba183da341e97702cd8ae91e4a894101eff347e971a"
 
   def install
     bin.install "upall"
