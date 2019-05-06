d4677b1c1beb355631dcf90975d1b660a1fdd490
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2019-05-06 01:07:39 -0300

    Bump OcrImageToTxt to 1.0.1

--- Formula/ocr-image-to-txt.rb
+++ Formula/ocr-image-to-txt.rb
@@ -2,8 +2,8 @@ class OcrImageToTxt < Formula
   desc "Extract using ocr text from a image"
   homepage "https://rogeriopradoj.github.io/homebrew-bin/#ocr-image-to-txt"
   url "https://raw.githubusercontent.com/rogeriopradoj/homebrew-bin/master/bin/ocr-image-to-txt"
-  version "1.0.0"
-  sha256 "54639ea242f69077bb1d0d96d835378a7c9d5baae48545558e56eac7ddd61123"
+  version "1.0.1"
+  sha256 "fdaaa625dafe2f39a8767e85a341d0a27303fc937fa09ae582894748f15a3060"
 
   def install
     bin.install "ocr-image-to-txt"
